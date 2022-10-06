454d696338cbce138f43bb6d91230542c3f7c49f
LryGean <897704091@qq.com>
2022-10-06 15:18:32 +0800

    code

---
+++ 爬虫/作业/17期爬虫-08-Scrapy框架-李容宇/BeiKe_scrapy/BeiKe_scrapy/pipelines.py
@@ -0,0 +1,30 @@
+# Define your item.txt pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item.txt types with a single interface
+import csv
+
+from itemadapter import ItemAdapter
+
+
+class BeikeScrapyPipeline:
+    def __init__(self):
+        self.writer = csv.writer(open('./BeiKe_scrapy/Data/贝壳找房.csv', 'w+', encoding='utf-8', newline=''))
+        self.headers = ('title', 'region', 'block', 'community', 'area', 'toward', 'type', 'rent', 'time')
+        self.writer.writerow(self.headers)
+
+    def process_item(self, item, spider):
+        rows = []
+        for header in self.headers:
+            rows.append(item[header])
+        rows = tuple(rows)
+        self.writer.writerow(rows)
+        print("Over！")
+        return item
+
+    def close_spider(self, spider):
+        print('关闭文件')
+        self.f.close()
