96a59705252e6c511c491e457719c760841b447a
LryGean <897704091@qq.com>
2022-10-09 07:45:01 +0800

    base64加密

---
+++ 爬虫/JS逆向/base64加密/base64加密.py
@@ -0,0 +1,16 @@
+import base64
+
+# # 加密  # ascll  或 二进制
+# 编码:   特征；有等号=
+# a = base64.b64encode(b'Gean')
+# print(a)
+#
+# b = base64.b64decode(a)
+# print(b)
+
+# b = 'eJx10dtKw0AQBuB3Wdi7JXs+JHcpjdhqxEMoSBGpaUxr7SkJpiq+u5NtU/DCq/l2dnb4Yb9RNZqjiDPGFCfoo6hQhHjAAoMIamq4MUY4E1qmtVME5X96hmlB0Es1GaJoqq0gJlRPXeMezlOupSHOQOdEARSKCD8zghG0aJpdHVHatm0wX842u+WmDPLtmtaL7Y6+m4tknGYr+ZU8SpcyiPT/g2JW5Qu6Kj7bbTWnnLJnnGgcJzhmOLHYhTgOu84gxqFFkGCddQm0YoSbEFYfZTtJBzJeFiS9DEh4ScK1O+v4QoDMWcqL9xKwTzMv2Ke6OaUskc5D91A9BJE+4qqLCHV2qk1/TuGXYLJelhtQMT5kV7UdXdNBWufskMtb7ko5vtnrz7fsQbjh5C4uB/vLtnhFP7/GJnv/'
+# b = base64.b64decode(b)
+#
+# print(b)
+# print(type(b))
+
